cf98e97700684e604e1f4d90f3ed77ea73c215e0
Bridget <bridgetlawani09@gmail.com>
2022-10-12 23:27:22 +0100

    initial commit of this project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React, { useState} from 'react'
-import TodoList from './TodoList'
+import React from 'react'
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import './App.css';
+import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
 
 function App() {
-  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
+  
   return (
     <>
-    <TodoList todos={todos}/>
-    <input type="text" />
-    <button>Add Todo</button>
-    <button>Clear Completed</button>
-    <p>0 left to do</p>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element ={<Home/>} />
+        <Route path='/services' element ={<Services/>} />
+        <Route path='/products' element ={<Products/>} />
+        <Route path='/sign-up' element ={<SignUp/>} />
+      </Routes>
+    </Router>
     </>
+
 
   );
 }
